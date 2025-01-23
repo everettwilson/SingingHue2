a2fe237445d0a4c3e97fac0e188654c959988e30
Everett Wilson <everettwilson21@me.com>
2025-01-23 14:52:38 -0600

    Initial Commit

---
+++ SingingHue2/SingingHue2App.swift
@@ -0,0 +1,17 @@
+//
+//  SingingHue2App.swift
+//  SingingHue2
+//
+//  Created by Everett Wilson on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SingingHue2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
